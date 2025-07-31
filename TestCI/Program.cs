c6ba74af7f52bd52cbc7b3ab5682a87b6fc7ba0f
dsy401 <oliverdeng1020@gmail.com>
2025-08-01 08:51:53 +1200

    update

--- TestCI/Program.cs
+++ TestCI/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var connectionString = Environment.GetEnvironmentVariable("connectionString");
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
 Console.WriteLine(connectionString);
