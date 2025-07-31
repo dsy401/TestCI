c8b7b5ebce99fde5181f861bc7807862cb768c60
dsy401 <oliverdeng1020@gmail.com>
2025-08-01 08:46:28 +1200

    first commit

---
+++ TestCI/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var connectionString = Environment.GetEnvironmentVariable("connectionString");
+
+Console.WriteLine(connectionString);
